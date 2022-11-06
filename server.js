020f16e8edcfc2e0a5d4083b4cd533418bc503fb
Ax52 <ax51@bk.ru>
2022-11-06 20:48:58 +0300

    Added backend server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "./dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App started on ${PORT} port`);
+});
