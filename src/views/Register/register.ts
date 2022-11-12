c778e38998e69167e74070c46fcd50e73220de99
ax52 <ax51@bk.ru>
2022-11-12 19:23:38 +0300

    Migrated Register page to the TS

---
+++ src/views/Register/register.ts
@@ -0,0 +1,30 @@
+import hbs from "./register.hbs";
+import css from "./register.module.scss";
+import { routeTo } from "../../utils";
+
+export function RegisterPage(root: HTMLElement) {
+  // render
+  root.innerHTML = hbs({ css });
+
+  //event listeners
+  const form = document.querySelector("#form-register");
+  if (form instanceof HTMLFormElement) {
+    form.onsubmit = (e) => {
+      e.preventDefault();
+    };
+  }
+
+  const cancelBtn = document.querySelector("#cancel-btn");
+  if (cancelBtn instanceof HTMLElement) {
+    cancelBtn.onclick = () => {
+      routeTo("/");
+    };
+  }
+
+  const registerBtn = document.querySelector("#register-btn");
+  if (registerBtn instanceof HTMLElement) {
+    registerBtn.onclick = () => {
+      routeTo("/");
+    };
+  }
+}
