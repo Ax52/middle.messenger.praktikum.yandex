e11d676b4a00f0c626d792fd82c116fe0df93b23
ax52 <ax51@bk.ru>
2022-11-12 19:25:20 +0300

    Migrated Settings Component to the TS

---
+++ src/views/Chats/Components/Settings/settings.ts
@@ -0,0 +1,37 @@
+import hbs from "./settings.hbs";
+import css from "./settings.module.scss";
+import { routeTo } from "~/src/utils";
+
+export function SettingsPage(root: HTMLElement) {
+  // render
+  root.innerHTML = hbs({ css });
+
+  // event listeners
+  const form = document.querySelector("#form-settings");
+  if (form instanceof HTMLFormElement) {
+    form.onsubmit = (e) => {
+      e.preventDefault();
+    };
+  }
+
+  const cancelBtn = document.querySelector("#cancel-btn");
+  if (cancelBtn instanceof HTMLElement) {
+    cancelBtn.onclick = () => {
+      routeTo("/chat");
+    };
+  }
+
+  const saveBtn = document.querySelector("#save-btn");
+  if (saveBtn instanceof HTMLElement) {
+    saveBtn.onclick = () => {
+      routeTo("/chat");
+    };
+  }
+
+  const logoutBtn = document.querySelector("#logout-btn");
+  if (logoutBtn instanceof HTMLElement) {
+    logoutBtn.onclick = () => {
+      routeTo("/");
+    };
+  }
+}
