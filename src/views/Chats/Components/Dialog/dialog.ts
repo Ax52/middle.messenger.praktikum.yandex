c657ca2ee18faa593e0a43502774f51c12b02e5c
ax52 <ax51@bk.ru>
2022-11-20 14:34:59 +0300

    Added reactivity to the dialog. Now you can send messages and view them on the screen

--- src/views/Chats/Components/Dialog/dialog.ts
+++ src/views/Chats/Components/Dialog/dialog.ts
@@ -1,6 +1,12 @@
 import Handlebars from "handlebars";
 import hbs from "./dialog.hbs";
 import css from "./dialog.module.scss";
+import {
+  Component,
+  validateForm,
+  formatTime,
+  TFormats,
+} from "../../../../utils";
 
 const username = "Sergio";
 const messages = [
@@ -24,9 +30,70 @@ const messages = [
   },
 ];
 
-export function Dialog(root: HTMLElement) {
-  Handlebars.registerHelper("isYourMessage", (v) => v !== "you");
+type TMessages = {
+  messages: Array<{
+    author: string;
+    time: string;
+    message: string;
+  }>;
+};
 
-  // render
-  root.innerHTML = hbs({ css, username, messages });
+export class Dialog extends Component {
+  root: HTMLElement;
+
+  override state: TMessages;
+
+  constructor(root: HTMLElement) {
+    super(root);
+    this.root = root;
+    this.state = this.init();
+  }
+
+  override init() {
+    // register helpers
+    Handlebars.registerHelper("isYourMessage", (v) => v !== "you");
+
+    // init state
+    const state: TMessages = { messages };
+    this.state = state;
+    return state;
+  }
+
+  pushMessage(text: string) {
+    const time = formatTime(new Date(), TFormats.short);
+    this.setState((prev: TMessages) => ({
+      messages: [
+        ...prev.messages,
+        {
+          author: "you",
+          time,
+          message: text,
+        },
+      ],
+    }));
+  }
+
+  async handleSubmit(e: SubmitEvent) {
+    const form = e.target as HTMLFormElement;
+    try {
+      await validateForm(e);
+      this.pushMessage(form.message.value);
+    } catch (err: unknown) {
+      console.error("Error with message form: ", err);
+    }
+  }
+
+  override addEvents() {
+    return [
+      {
+        event: "submit",
+        targetId: "#form-message",
+        callback: (e: SubmitEvent) => this.handleSubmit(e),
+      },
+    ];
+  }
+
+  override render() {
+    return hbs({ css, username, messages: this.state.messages });
+  }
 }
