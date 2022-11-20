701be83bf179e362b0d8425ece8a869d8c08ccb5
ax52 <ax51@bk.ru>
2022-11-20 14:31:39 +0300

    Created united validateForm function

---
+++ src/utils/formValidation/validateForm.ts
@@ -0,0 +1,53 @@
+import { formValidatior, TFormData } from "./formValidator";
+import { EventBus } from "../EventBus";
+import css from "../utils.module.scss";
+
+export function validateForm(event: SubmitEvent) {
+  event.preventDefault();
+
+  const form = event.target as HTMLFormElement;
+
+  return new Promise<void>((res, rej) => {
+    if (!form) {
+      rej();
+    }
+
+    const invalidFields = formValidatior([
+      ...new FormData(form as HTMLFormElement),
+    ] as TFormData[]);
+
+    if (invalidFields.length) {
+      invalidFields.forEach(([fieldName]) => {
+        const input = form.elements.namedItem(fieldName) as HTMLInputElement;
+        input.classList.add(css["input-error"] as string);
+
+        const eventBus = new EventBus(input);
+
+        const listen = (evt: string) =>
+          function checkVal() {
+            const val = input.value;
+            const inputValidation = formValidatior([[fieldName, val]]);
+            if (!inputValidation.length) {
+              input.classList.remove(css["input-error"] as string);
+              eventBus.off(evt, checkVal);
+            }
+          };
+
+        /** NOTE:
+         * onBlur/onFocus validations added only after submit check fails. and only on invalid fields.
+         * it works almost the same as react-hook-form does. It allows to enter any information in the field
+         * and doesn't warns before you double check form and tap "submit" button. With this logic we doesn't
+         * get any warns on empty fields focus. Futhermore, if field is checked as invalid after submit, it
+         * starts to listen every input change and dynamically says that user fixed the error
+         */
+        eventBus.on("focus", listen("focus"));
+        eventBus.on("input", listen("input"));
+        eventBus.on("blur", listen("blur"));
+      });
+    } else {
+      // NOTE: for task purposes only, added oneliner for console.logging form data
+      console.log(Object.fromEntries([...new FormData(form)])); // FIXIT: delete this line after review
+      res();
+    }
+  });
+}
