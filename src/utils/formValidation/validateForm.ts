b9b0a9cfa587b895227a66a6016832ddd2ad340a
ax52 <ax51@bk.ru>
2022-12-17 15:25:05 +0300

    validateForm: minor types improvements

--- src/utils/formValidation/validateForm.ts
+++ src/utils/formValidation/validateForm.ts
@@ -2,12 +2,14 @@ import { formValidatior, TFormData } from "./formValidator";
 import { EventBus } from "../EventBus";
 import css from "../utils.module.scss";
 
-export function validateForm(event: SubmitEvent) {
+export function validateForm(
+  event: SubmitEvent,
+): Promise<Record<string, FormDataEntryValue>> {
   event.preventDefault();
 
   const form = event.target as HTMLFormElement;
 
-  return new Promise<void>((res, rej) => {
+  return new Promise((res, rej) => {
     if (!form) {
       rej();
     }
@@ -45,7 +47,10 @@ export function validateForm(event: SubmitEvent) {
         eventBus.on("blur", listen("blur"));
       });
     } else {
-      res();
+      const formObject = [...new FormData(form)].reduce<
+        Record<string, FormDataEntryValue>
+      >((a, [key, val]) => ({ ...a, [key]: val }), {});
+      res(formObject);
     }
   });
 }
