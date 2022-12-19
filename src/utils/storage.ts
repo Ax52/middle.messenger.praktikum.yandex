d2bacad69589a881c7281baae66e69226b65eb18
ax52 <ax51@bk.ru>
2022-12-19 12:45:45 +0300

    Storage util: added update method

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -24,4 +24,28 @@ export const storage = {
       localStorage.setItem(key, valToStore);
     }
   },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  update(key: string, value: any) {
+    const prevVal = this.get(key);
+    if (prevVal) {
+      if (Array.isArray(value)) {
+        // eslint-disable-next-line no-param-reassign
+        value = prevVal.concat(value);
+      } else if (typeof value === "object") {
+        // eslint-disable-next-line no-param-reassign
+        value = { ...prevVal, value };
+      }
+    }
+    let valToStore = value;
+    if (typeof value === "object") {
+      try {
+        valToStore = JSON.stringify(value);
+      } catch (err) {
+        console.warn("Failed to stringify value:\n", value, "\n", err);
+      }
+    }
+    if (this.get(key) !== valToStore) {
+      localStorage.setItem(key, valToStore);
+    }
+  },
 };
