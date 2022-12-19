bd3dc22423ea888959bb8a4e0142a706daffc3d9
ax52 <ax51@bk.ru>
2022-12-19 12:44:33 +0300

    Dialog reactivity enabled

--- src/views/Chats/Components/Dialog/dialog.ts
+++ src/views/Chats/Components/Dialog/dialog.ts
@@ -7,74 +7,119 @@ import {
   formatTime,
   TFormats,
   getChatId,
+  OpenWS,
+  storage,
 } from "../../../../utils";
+import { ChatApi } from "../../../../API";
 
-const username = "Sergio";
-const messages = [
-  {
-    author: "Sergio",
-    time: "21:12",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    author: "you",
-    time: "23:55",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    author: "Sergio",
-    time: "02:43",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-];
-
-type TMessages = {
-  messages: Array<{
+type TState = {
+  title?: string;
+  messages?: Array<{
     author: string;
     time: string;
     message: string;
   }>;
 };
 
-export class Dialog extends Component {
+type TWSMessage = {
+  content: string;
+  type: string;
+  time: string;
+  user_id: number;
+};
+
+export class Dialog extends Component<TState> {
+  override state: TState = {
+    title: "",
+    messages: storage.get("messages") ?? [],
+  };
+
   root: HTMLElement;
 
-  override state: TMessages;
-
   dialogId: string;
+
+  userId?: number;
+
+  ws?: WebSocket;
 
   constructor(root: HTMLElement) {
     super(root);
     this.root = root;
-    this.state = this.init();
     this.dialogId = getChatId();
+
+    this.#initDialog();
+    this.init();
+
+    this.setState = this.setState.bind(this);
   }
+
+  async #updateDialogTitle() {
+    try {
+      const chats = await ChatApi.getChats();
+      const { title } = chats.find(
+        ({ id }: { id: number }) => id === Number(this.dialogId),
+      );
+      this.setState((prev) => (prev ? { ...prev, title } : { title }));
+    } catch (err) {
+      console.warn("failed to update Dialog Title");
+    }
+  }
+
+  async #initUser() {
+    try {
+      const { id } = await ChatApi.getUser();
+      this.userId = id;
+    } catch (err) {
+      console.error("Failed to fetch user info: ", err);
+    }
+  }
+
+  async #initDialog() {
+    this.#updateDialogTitle();
+    this.#initUser();
+    this.ws = await OpenWS({
+      chatId: this.dialogId,
+      onopen: this.log,
+      onclose: this.handleClose,
+      onmessage: this.updateMessages,
+      onerror: this.log,
+    });
+  }
+
+  log = (e: Event) => {
+    console.log("[log]: ", e, this);
+  };
 
   override init() {
     // register helpers
     Handlebars.registerHelper("isYourMessage", (v) => v !== "you");
-
-    // init state
-    const state: TMessages = { messages };
-    this.state = state;
-    return state;
   }
 
-  pushMessage(text: string) {
-    const time = formatTime(new Date(), TFormats.short);
-    this.setState((prev: TMessages) => ({
-      messages: [
-        ...prev.messages,
-        {
-          author: "you",
-          time,
-          message: text,
-        },
-      ],
-    }));
+  updateMessages = ({ content, type, time, user_id: id }: TWSMessage) => {
+    if (type === "message") {
+      const newRecord = {
+        author: `${id === this.userId ? "you" : id}`,
+        time: formatTime(new Date(time), TFormats.short),
+        message: content,
+      };
+      this.setState((prev) => ({
+        ...prev,
+        messages: prev?.messages ? [...prev.messages, newRecord] : [newRecord],
+      }));
+      storage.update("messages", [newRecord]);
+    }
+  };
+
+  handleClose = () => {
+    this.#initDialog();
+  };
+
+  async pushMessage(text: string) {
+    try {
+      this.ws?.send(JSON.stringify({ content: text, type: "message" }));
+    } catch (err) {
+      console.error("Failed to send message via WS: ", err);
+    }
   }
 
   async handleSubmit(e: SubmitEvent) {
@@ -87,17 +132,51 @@ export class Dialog extends Component {
     }
   }
 
+  async addUserToChat() {
+    const userId = prompt("Enter user ID");
+    try {
+      await ChatApi.addUserToChat(Number(userId), Number(this.dialogId));
+    } catch (err) {
+      console.error("failed to add user: ", err);
+    }
+  }
+
+  async removeUserFromChat() {
+    const userId = prompt("Enter user ID");
+    try {
+      await ChatApi.removeUserFromChat(Number(userId), Number(this.dialogId));
+    } catch (err) {
+      console.error("failed to add user: ", err);
+    }
+  }
+
   override addEvents() {
     return [
       {
         event: "submit",
         targetId: "#form-message",
-        callback: (e: SubmitEvent) => this.handleSubmit(e),
+        callback: (e: SubmitEvent) => {
+          this.handleSubmit(e);
+        },
+      },
+      {
+        event: "click",
+        targetId: "#add-user",
+        callback: () => this.addUserToChat(),
+      },
+      {
+        event: "click",
+        targetId: "#del-user",
+        callback: () => this.removeUserFromChat(),
       },
     ];
   }
 
   override render() {
-    return hbs({ css, username, messages: this.state.messages });
+    return hbs({
+      css,
+      title: this.state?.title ?? "Dialog",
+      messages: this.state?.messages ?? [],
+    });
   }
 }
