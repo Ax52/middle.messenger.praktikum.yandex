1cea0cfc2bc039a68839666094041e7943b835e6
ax52 <ax51@bk.ru>
2022-12-22 18:14:54 +0300

    Added defence agaings dos attacks

--- src/utils/MyAxios.ts
+++ src/utils/MyAxios.ts
@@ -17,6 +17,20 @@ interface IOptions {
   query?: Record<string, string>;
 }
 
+let callStack = 0;
+
+// NOTE: DOS guard
+const dosGuard = (wasting = 3_000, parallelLimit = 25) => {
+  callStack++;
+  setTimeout(() => {
+    callStack--;
+  }, wasting);
+  if (callStack > parallelLimit) {
+    return false;
+  }
+  return true;
+};
+
 const request = (
   url: string,
   { method, responseType, data, timeout, headers }: IOptions = {
@@ -24,42 +38,45 @@ const request = (
     method: Methods.get,
   },
 ) =>
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  new Promise<any>((res, rej) => {
-    const xml = new XMLHttpRequest();
-    xml.open(method as string, new URL(url));
-    xml.withCredentials = true;
-    if (headers && headers?.length) {
-      headers.forEach(([name, val]) => {
-        xml.setRequestHeader(name, val);
-      });
-    }
-    xml.responseType = responseType ?? "text";
-    xml.timeout = timeout as number;
-    xml.onload = () => {
-      if (xml.status >= 200 && xml.status <= 299) {
-        try {
-          const parsed = JSON.parse(xml.response);
-          res(parsed);
-        } catch {
-          res(xml.response);
+  dosGuard()
+    ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      new Promise<any>((res, rej) => {
+        const xml = new XMLHttpRequest();
+        xml.open(method as string, new URL(url));
+        xml.withCredentials = true;
+        if (headers && headers?.length) {
+          headers.forEach(([name, val]) => {
+            xml.setRequestHeader(name, val);
+          });
         }
-      } else if (xml.responseText) {
-        try {
-          const { reason } = JSON.parse(xml.responseText);
-          rej(reason);
-        } catch {
+        xml.responseType = responseType ?? "text";
+        xml.timeout = timeout as number;
+        xml.onload = () => {
+          if (xml.status >= 200 && xml.status <= 299) {
+            try {
+              const parsed = JSON.parse(xml.response);
+              res(parsed);
+            } catch {
+              res(xml.response);
+            }
+          } else if (xml.responseText) {
+            try {
+              const { reason } = JSON.parse(xml.responseText);
+              rej(reason);
+            } catch {
+              rej(xml);
+            }
+          } else {
+            rej(xml);
+          }
+        };
+        xml.onerror = () => {
           rej(xml);
-        }
-      } else {
-        rej(xml);
-      }
-    };
-    xml.onerror = () => {
-      rej(xml);
-    };
-    xml.send(data);
-  });
+        };
+        xml.send(data);
+      })
+    : // eslint-disable-next-line prefer-promise-reject-errors
+      Promise.reject("Too many requests");
 
 export class Axios {
   static get = (url: string, options: IOptions = { timeout: 5000 }) => {
