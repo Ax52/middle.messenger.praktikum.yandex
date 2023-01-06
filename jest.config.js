751641a3c65ba366ed418fd0b77c00e014020203
ax52 <ax51@bk.ru>
2023-01-06 14:57:06 +0300

    feat: added jest tests to the project

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+  },
+};
