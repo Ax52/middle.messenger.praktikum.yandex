a939e3fefd48fcc199be810b119b112125f299f3
ax52 <ax51@bk.ru>
2023-01-06 14:58:00 +0300

    chore: added tests for ChatApi

---
+++ src/API/ChatApi.test.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ChatApi } from "./ChatApi";
+
+test("Should check login user", async () => {
+  const res = await ChatApi.checkAccess();
+  expect(res).toBe(false);
+});
+
+test("Should register user", async () => {
+  let attempts = 3;
+  let res: any;
+  async function attempt() {
+    try {
+      if (!attempts) {
+        res = undefined;
+        return;
+      }
+      const fakeData = {
+        first_name: "string",
+        second_name: "string",
+        login: `string${Math.round(Math.random() * 100)}`,
+        email: `string${Math.round(Math.random() * 100)}@asd.asd`,
+        password: "stringString123",
+        phone: "0123456789",
+      };
+      res = await ChatApi.register(fakeData);
+    } catch {
+      attempts--;
+      attempt();
+    }
+  }
+  await attempt();
+
+  expect(typeof res?.id).toBe("number");
+});
